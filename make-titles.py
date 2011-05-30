ea03d0b831f2ca004c3c8b56871d10de09f74a3a
ubershmekel <ubershmekel@localhost>
2011-05-30 19:23:41 +0000

    Fixed for python 3.2 and 2.7

--- make-titles.py
+++ make-titles.py
@@ -14,18 +14,20 @@ TitleExample.prtl - this title file you can redesign as you like, only
 No rights reserved on this code unless required by law.
 
 btw Version 2.0 will be able to take srt files and make an importable sequence!
+
+works on Python 2.7 and Python 3.2
 '''
 import re
 
 # this is useful for RTL languages like hebrew, turn this to False for english.
 REVERSE_TEXT = True
 
-TEMPLATE = open("TitleExample.prtl").read()
+TEMPLATE = open("TitleExample.prtl", "rb").read()
 TEMPLATE = TEMPLATE.decode('utf-16')
 # this file should be unix format (only \n, not \r\n, because I split on \n\n)
-SUBS_TEXT = open("Subtitles.txt").read().decode('utf-8')[1:]
-FIRST_LINE_TO_REPLACE = u'FirstLine'
-SECOND_LINE_TO_REPLACE = u'SecondLine'
+SUBS_TEXT = open("Subtitles.txt", "rb").read().decode('utf-8')[1:]
+FIRST_LINE_TO_REPLACE = 'FirstLine'
+SECOND_LINE_TO_REPLACE = 'SecondLine'
 
 def MakePrtlFile(textlines, fname):
     # premiere does newlines in a very strange fashion... So i just remove theme
@@ -34,30 +36,24 @@ def MakePrtlFile(textlines, fname):
     if len(lines) > 1:
         secondLine = lines[1]
     else:
-        secondLine = u''
+        secondLine = ''
         
     if REVERSE_TEXT:
         secondLine = secondLine[::-1]
         firstLine = firstLine[::-1]
         
-    print fname
-    print repr(firstLine)
-    print repr(secondLine)
+    print(fname)
+    # complicated unicode escaping for when printing  to an ascii terminal
+    print(firstLine.encode('unicode_escape').decode('ascii'))
+    print(secondLine.encode('unicode_escape').decode('ascii'))
 
-##	textlines = "\n".join([line[::-1] for line in textlines.split('\n')])
-##
-##	parsedline = textlines.replace(u'\r\n', u' ')
-##	parsedline = parsedline.replace(u'\n', u' ')
-##	print repr(parsedline)
-    
-    
-    # This is the amount of characters + 1, needs to be replaced or else premiere crashes
-    runcountLineOne = u'RunCount="%d"' % (len(firstLine) + 1)
-    runcountLineOneA = u'RunCountA="%d"' % (len(firstLine) + 1)
-    runcountLineTwo = u'RunCount="%d"' % (len(secondLine) + 1)
-    runcountLineTwoB = u'RunCountB="%d"' % (len(secondLine) + 1)
-    newt = TEMPLATE.replace(u'RunCount="10"', runcountLineOneA)
-    newt = newt.replace(u'RunCount="11"', runcountLineTwoB)
+    # RunCount is the amount of characters + 1, needs to be replaced or else premiere crashes
+    runcountLineOne = 'RunCount="%d"' % (len(firstLine) + 1)
+    runcountLineOneA = 'RunCountA="%d"' % (len(firstLine) + 1)
+    runcountLineTwo = 'RunCount="%d"' % (len(secondLine) + 1)
+    runcountLineTwoB = 'RunCountB="%d"' % (len(secondLine) + 1)
+    newt = TEMPLATE.replace('RunCount="10"', runcountLineOneA)
+    newt = newt.replace('RunCount="11"', runcountLineTwoB)
     newt = newt.replace(runcountLineOneA, runcountLineOne)
     newt = newt.replace(runcountLineTwoB, runcountLineTwo)
     
@@ -65,7 +61,7 @@ def MakePrtlFile(textlines, fname):
     newt = newt.replace(FIRST_LINE_TO_REPLACE, firstLine)
     newt = newt.replace(SECOND_LINE_TO_REPLACE, secondLine)    
 
-    f = open(fname, "w")
+    f = open(fname, "wb")
 
     # if we don't make the file in the correct encoding, premiere crahes.
     f.write(newt.encode('utf-16'))
@@ -74,7 +70,7 @@ def MakePrtlFile(textlines, fname):
 def ParseSubsFile():
 	subs = [x for x in SUBS_TEXT.split('#') if len(x) > 0]
 	
-	print len(subs)
+	print(len(subs))
 	for i, text in enumerate(subs):
 		num_id = "%04d" % i
 		fname = "Subs\\Title%s%s" % (num_id, ".prtl")
